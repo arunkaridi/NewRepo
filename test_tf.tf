b2bc6632fb5d961db8797e06665cf6d52a217805
Arun Karidi <arunkaridi@gmail.com>
2023-10-29 20:02:14 +0530

    modified:   test_tf.tf

--- test_tf.tf
+++ test_tf.tf
@@ -2,3 +2,8 @@ resource "local_file" "hello_world"{
     filename = "C:\\Documents\\hello_world.txt"
     content = "Hello World! 1 2 3 4 5 6 7 8 "
 }
+
+resource "local_file" "hello_1"{
+    filname = "C:\\Documents\\hello_1.txt"
+    content = "Hello 1 2 3 test"
+}
